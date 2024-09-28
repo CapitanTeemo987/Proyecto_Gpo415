e2e9d207cdf7b196d08a12ca6db37bc7f367a8ad
Valentino Ortiz <A01712296@tec.mx>
2024-09-27 23:40:38 -0300

    Add files via upload

--- conversor.py
+++ conversor.py
@@ -1,3 +1,14 @@
+historial_conversiones = []
+
+def mostrar_historia():
+    if historial_conversiones:
+        print("\n__Historial de conversiones__")
+        for i in historial_conversiones:
+            print(f"\n{i}")
+    else:
+        print("\nNo hay conversiones registradas")
+
+
 def km_a_milla(km):
     operacion = km * 0.621371
     return operacion
@@ -32,29 +43,33 @@ Menu de Longitud
         if respuesta2 == 1:
             km = float(input("Escribe los km: "))
             resultado1 = km_a_milla(km)
+            historial_conversiones.append(f"{km} km son {resultado1} millas")
             print(f"El total en millas es: {resultado1}")
 
         elif respuesta2 == 2:
             mi = float(input("Escriba las millas: "))
             resultado2 = milla_a_km(mi)
+            historial_conversiones.append(f"{mi} mi son {resultado2} km")
             print(f"El total en km es: {resultado2}")
 
         elif respuesta2 == 3:
             Cm = float(input("Escribe los Cm: "))
             resultado3 = Cm_a_Metros(Cm)
+            historial_conversiones.append(f"{Cm} Cm son {resultado3} Metros")
             print(f"El total de Cm a Metros es: {resultado3}")
 
         elif respuesta2 == 4:
             Metros = float(input("Escribe la cantidad de Metros: "))
             resultado4 = Metros_a_Cm(Metros)
+            historial_conversiones.append(f"{Metros} Metros son {resultado4} Cm")
             print(f"El total en Metros es: {resultado4}")
 
         elif respuesta2 == 5:
-            print("Regresando al menu principal...")
+            print("\nRegresando al menu principal...")
             break
 
         else:
-            print("selecciona una opción del 1-3")
+            print("\nselecciona una opción del 1-3")
 
 
 def Seg_a_Min(Seg):
@@ -88,29 +103,33 @@ Menu de Tiempo
         if respuesta3 == 1:
             Seg = float(input("Escribe los Seg: "))
             resultado1 = Seg_a_Min(Seg)
+            historial_conversiones.append(f"{Seg} Seg son {resultado1} Min")
             print(f"El total en Minutos es: {resultado1}")
 
         elif respuesta3 == 2:
             Min = float(input("Escribe la cantidad de Min: "))
             resultado2 = Min_a_Seg(Min)
+            historial_conversiones.append(f"{Min} Min son {resultado2} Seg")
             print(f"La cantidad en Segundos es: {resultado2}")
 
         elif respuesta3 == 3:
             Mic = float(input("Ingresa la cantidad de Microsegundos: "))
             resultado3 = Mic_a_Mil(Mic)
+            historial_conversiones.append(f"{Mic} Microsegundos son {resultado3} Milisegundos")
             print(f"El resultado en Milisegudnos es: {resultado3}")
 
         elif respuesta3 == 4:
             Mil = float(input("Escribe la cantidad de Milisegudnos: "))
             resultado4 = Mil_a_Mic(Mil)
+            historial_conversiones.append(f"{Mil} Milisegundos son {resultado4} Microsegundos")
             print(f"El resultado en Microsegundos es: {resultado4}")
 
         elif respuesta3 == 5:
-            print("Regresando al menu principal...")
+            print("\nRegresando al menu principal...")
             break
 
         else:
-            print("Escribe una opción del 1-5")
+            print("\nEscribe una opción del 1-5")
 
 def Cm2_a_Metro2(Cm2):
     operacion = Cm2 * 10**-4
@@ -145,29 +164,33 @@ Menu Area
         if respuesta4 == 1:
             Cm2 = float(input("Escribe los Cm2: "))
             resultado1 = Cm2_a_Metro2(Cm2)
+            historial_conversiones.append(f"{Cm2} Cm2 son {resultado1} Metros2")
             print(f"El total es de: {resultado1:.4f} Metros")
 
         elif respuesta4 == 2:
             Metro2 = float(input("Escribe los Metros2: "))
             resultado2 = Metro2_a_Cm2(Metro2)
+            historial_conversiones.append(f"{Metro2} Metros2 son {resultado2} Cm2")
             print(f"El total es de: {resultado2} Cm2")
 
         elif respuesta4 == 3:
             Km2 = float(input("Escribe los Km2: "))
             resultado3 = Km2_a_Ha(Km2)
+            historial_conversiones.append(f"{Km2} km2 son {resultado3} Ha")
             print(f"El total es de: {resultado3} Ha")
 
         elif respuesta4 == 4:
             Ha = float(input("Escribe las Hectareas: "))
             resultado4 = Ha_a_Km2(Ha)
+            historial_conversiones.append(f"{Ha} Hectareas son {resultado4} Km2")
             print(f"El total es de: {resultado4} Km2")
 
         elif respuesta4 == 5:
-            print("Regresando al menu principal...")
+            print("\nRegresando al menu principal...")
             break
 
         else:
-            print("Escribe una opcion del 1-5")
+            print("\nEscribe una opcion del 1-5")
 
 while True:
     print("""
@@ -176,7 +199,8 @@ while True:
     [1] Longitud
     [2] Tiempo
     [3] Area
-    [4] Salir
+    [4] Historial
+    [5] Salir
     """)
 
     respuesta = int(input("Escribe la opción deseada: "))
@@ -184,12 +208,16 @@ while True:
     if respuesta == 1:
         menu_longitud()
 
-    if respuesta == 2:
+    elif respuesta == 2:
         menu_tiempo()
 
-    if respuesta == 3:
+    elif respuesta == 3:
         menu_area()
 
     elif respuesta == 4:
+        mostrar_historia()
+    
+    elif respuesta == 5:
         print("Saliendo del programa...")
         break
+    
